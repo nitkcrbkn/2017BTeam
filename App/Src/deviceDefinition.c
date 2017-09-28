31896bed3cc0449b02606b1ef05457045b877aa8
arukein <arukein00791@gmail.com>
2017-09-28 10:54:33 +0900

    実験のためMDのアドレスの変更

--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -6,14 +6,6 @@
 #if DD_NUM_OF_MD
 /*MD Definition*/
 DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
-  { .add  = 0x10, /* address */
-    .duty = 0, /* default duty */
-    .mode = D_MMOD_FREE, /* mode */
-  },
-  { .add  = 0x11, /* address */
-    .duty = 0, /* default duty */
-    .mode = D_MMOD_FREE, /* mode */
-  },
   { .add  = 0x12, /* address */
     .duty = 0, /* default duty */
     .mode = D_MMOD_FREE, /* mode */
@@ -22,12 +14,20 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
     .duty = 0, /* default duty */
     .mode = D_MMOD_FREE, /* mode */
   },
+  { .add  = 0x14, /* address */
+    .duty = 0, /* default duty */
+    .mode = D_MMOD_FREE, /* mode */
+  },
+  { .add  = 0x15, /* address */
+    .duty = 0, /* default duty */
+    .mode = D_MMOD_FREE, /* mode */
+  },
 };
 #endif
 #if DD_NUM_OF_AB
 /*AB Definition*/
 DD_ABHand_t g_ab_h[DD_NUM_OF_AB] = {
-  { .add = 0x20, /* address */
+  { .add = 0x21, /* address */
     .dat = 0x00, /* data */
   },              
 };
