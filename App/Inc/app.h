aea57a7eeefc4e94df066a521c0d9b9eab1fba3c
arukein <arukein00791@gmail.com>
2017-09-28 17:34:30 +0900

    実験による変更

--- App/Inc/app.h
+++ App/Inc/app.h
@@ -42,11 +42,13 @@ typedef enum {
 #define ARM_AB_MAX_COUNT 300
 */
 /*アーム用シリンダ*/
-#define ARM_MOVE (1<<1)
-
-/*ミサイル*/
 /*
-#define MISSILE_AB_0 (1<<2)
+#define ARM_MOVE (1<<1)
+*/
+/*ミサイル*/
+
+#define MISSILE_AB_0 (1<<1)
+/*
 #define MISSILE_AB_1 (1<<3)
 #define MISSILE_AB_2 (1<<4)
 */
