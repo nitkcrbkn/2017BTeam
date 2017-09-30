4ab8cc7d921a84cb9975280eb3e39cb1881c6360
arukein <arukein00791@gmail.com>
2017-09-30 09:59:28 +0900

    アドレスの変更

--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -6,19 +6,19 @@
 #if DD_NUM_OF_MD
 /*MD Definition*/
 DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
+  { .add  = 0x10, /* address */
+    .duty = 0, /* default duty */
+    .mode = D_MMOD_FREE, /* mode */
+  },
+  { .add  = 0x11, /* address */
+    .duty = 0, /* default duty */
+    .mode = D_MMOD_FREE, /* mode */
+  },
   { .add  = 0x12, /* address */
     .duty = 0, /* default duty */
     .mode = D_MMOD_FREE, /* mode */
   },
   { .add  = 0x13, /* address */
-    .duty = 0, /* default duty */
-    .mode = D_MMOD_FREE, /* mode */
-  },
-  { .add  = 0x14, /* address */
-    .duty = 0, /* default duty */
-    .mode = D_MMOD_FREE, /* mode */
-  },
-  { .add  = 0x15, /* address */
     .duty = 0, /* default duty */
     .mode = D_MMOD_FREE, /* mode */
   },
