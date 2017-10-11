77d06742329113723fc18962b674899fc51a258b
arukein <arukein00791@gmail.com>
2017-10-11 18:37:06 +0900

    モード変更の追加

--- App/Src/app.c
+++ App/Src/app.c
@@ -25,6 +25,14 @@ int moveAB(void);
 static
 int missileAB(void);
 
+static
+int changeOpeMode(void);
+
+static
+ope_mode_t g_ope_mode = OPE_MODE_N;
+
+static
+int suspensionSystem_F(void);
 /*メモ
  *g_ab_h...ABのハンドラ
  *g_md_h...MDのハンドラ
@@ -54,10 +62,22 @@ int appTask(void){
   /*それぞれの機構ごとに処理をする*/
   /*途中必ず定数回で終了すること。*/
 
-  ret = suspensionSystem();
+  switch(g_ope_mode){
+ 
+  case OPE_MODE_N:
+    ret = suspensionSystem();
     if(ret){
       return ret;
     }
+    break;
+  
+  case OPE_MODE_F:
+    ret = suspensionSystem_F();
+    if(ret){
+      return ret;
+    }
+    break;
+  }
  
     ret = armAB();
     if(ret){
@@ -192,6 +212,78 @@ int suspensionSystem(void){
     w = 0;
   }else{
     w = DD_RCGetRX(g_rc_data);
+  }
+  
+  
+  /*for each motor*/
+  for(i=0;i<num_of_motor;i++){
+    /*それぞれの差分*/
+    switch(i){
+    case 0:
+      idx = MECHA1_MD1;
+      m = -2*1/SR_SIX*y - 1*1/SR_THREE*w;
+      break;
+    case 1:
+      idx = MECHA1_MD2;
+      m = -1*1/SR_TWO*x + 1*1/SR_SIX*y - 1*1/SR_THREE*w;
+      break;
+    case 2:
+      idx = MECHA1_MD3;
+      m = 1*1/SR_TWO*x + 1*1/SR_SIX*y - 1*1/SR_THREE*w;
+      break;
+    default:
+      return EXIT_FAILURE;
+    }
+    m *= 95;
+    trapezoidCtrl(m,&g_md_h[idx],&tc);
+  }
+  
+  return EXIT_SUCCESS;
+
+}
+
+/*モード変更*/
+static
+int changeOpeMode(void){
+  
+  if(__RC_ISPRESSED_CIRCLE(g_rc_data)){
+    g_ope_mode = OPE_MODE_N;
+  }
+  else if(__RC_ISPRESSED_CROSS(g_rc_data)){
+    g_ope_mode = OPE_MODE_F;
+  }
+
+  return EXIT_SUCCESS;
+}
+
+/*プライベート 足回りシステム*/
+static
+int suspensionSystem_F(void){
+  const tc_const_t tc ={
+    .inc_con = 300,//DUTY上限時の傾き
+    .dec_con = 400//　　下限時
+  };
+  const int num_of_motor = 3;/*モータの個数*/
+  unsigned int idx;/*インデックス*/
+  int m,x,y,w;
+  int i;
+  
+  if(abs(DD_RCGetLX(g_rc_data))<CENTRAL_THRESHOLD){
+    y = 0;
+  }else{
+    y = DD_RCGetLX(g_rc_data);
+  }
+
+  if(abs(DD_RCGetLY(g_rc_data))<CENTRAL_THRESHOLD){
+    x = 0;
+  }else{
+    x = -DD_RCGetLY(g_rc_data);
+  }
+  
+  if(abs(DD_RCGetRX(g_rc_data))<CENTRAL_THRESHOLD){
+    w = 0;
+  }else{
+    w = -DD_RCGetRX(g_rc_data);
   }
   
   
